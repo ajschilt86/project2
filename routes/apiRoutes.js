a73e1e02615c31ea0bb93ad022fa8fb2acb5bb9b
Cloud Strife <mokryan17@gmail.com>
2018-09-23 00:52:33 -0500

    Completed apiRoutes so they include both models

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,29 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  // This should allow us to look at the full squadron w/ the planes through a left outer join
+  // Left outer join comes from the include
   app.get("/api/squadron", function(req, res) {
-    db.Squadron.findAll({}).then(function(dbSquadron) {
-      res.json(dbSquadron);
-    });
-  });
-
-  app.get("api/squadron/:name", function(req, res) {
-    db.Squadron.findOne({
-      where: {
-        name: req.params.name
-      }
+    db.Squadron.findAll({
+      include: [db.Planes]
     }).then(function(dbSquadron) {
       res.json(dbSquadron);
     });
   });
 
-  // Create a new example
+  // This will allow us to look up one specific squadron along w/ planes
+  app.get("api/squadron/:name", function(req, res) {
+    db.Squadron.findOne({
+      where: {
+        name: req.params.name
+      },
+      include: [db.Planes]
+    }).then(function(dbSquadron) {
+      res.json(dbSquadron);
+    });
+  });
+
+  // Create a new squadron
   app.post("/api/squadron", function(req, res) {
     db.Squadron.create(req.body).then(function(dbSquadron) {
       res.json(dbSquadron);
