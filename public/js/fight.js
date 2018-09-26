880c3b564546f34b7657a18ec22a2de7fab58498
Chaelor <joshsblizzard@gmail.com>
2018-09-26 12:54:15 -0500

    On fight screen, made button go to create page. On create added a fight button to the modal. Hopefully photos now working

--- public/js/fight.js
+++ public/js/fight.js
@@ -36,7 +36,7 @@ var planes = [
   {
     'UID': 3,
     'name': 'F-15 Eagle',
-    'img': '/img/f-15.jpg',
+    'img': '/img/f-15.JPG',
     'healthMax': 100, 
     'health': 100,
     'attackValueMax': 12,
@@ -156,7 +156,7 @@ var planes = [
   {
     'UID': 11,
     'name': 'JDG-00X Devil Gundam',
-    'img': '/img/devilgundam.png',
+    'img': '/img/DevilGundam_profile.png',
     'healthMax': 666,
     'health': 666,
     'attackValueMax': 205,
